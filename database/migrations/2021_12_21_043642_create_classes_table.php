446b5966f9422916825b842fbf299c90111270ab
AriKurniawan25 <arikn1205@gmail.com>
2021-12-21 11:43:56 +0700

    migration

--- database/migrations/2021_12_21_043642_create_classes_table.php
+++ database/migrations/2021_12_21_043642_create_classes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateClassesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('classes', function (Blueprint $table) {
             $table->id();
-            $table->string('email', 150)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 150);
-            $table->string('name', 50);
-            $table->string('role', 10);
-            $table->rememberToken();
+            $table->string('name', 150);
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('classes');
     }
 }
